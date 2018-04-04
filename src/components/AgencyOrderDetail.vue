e5ad1ecfb11b78a0d44aeaf52a2a088845ab6614
sRect <srect2017@gmail.com>
2018-04-04 23:18:47 +0800

    订单详情页增加同意退货逻辑

--- src/components/AgencyOrderDetail.vue
+++ src/components/AgencyOrderDetail.vue
@@ -55,12 +55,23 @@
           label="订单状态"
           :formatter="formatterOrderStatus"
           sortable>
+          <template slot-scope="scope">
+            <el-button 
+              :disabled="scope.row.orderStatus !== 4"
+              type="primary" 
+              size="small"
+              @click="handleTuiHuo(scope.row)">
+              {{ scope.row.orderStatus | filterOrderStatus(scope.row.orderStatus) }}
+            </el-button>
+          </template>
         </el-table-column>
       </el-table>
   </div>
 </template>
 
 <script>
+  import debounce from 'lodash/debounce'
+
   export default {
     name: 'AgencyOrderDetail',
     data() {
@@ -69,14 +80,39 @@
         tableData: []
       }
     },
+    filters: {
+      filterOrderStatus(val) {
+        let num = Number.parseInt(val);
+        switch(num) {
+            case 0:
+              return "配货";
+              break;
+            case 1:
+              return "运输中";
+              break;
+            case 2:
+              return "运输完毕";
+              break;
+            case 4:
+              return "同意退货";
+              break;
+            case 5:
+              return "退货完毕";
+              break;
+            default:
+              return '--';
+              break;
+          }
+      }
+    },
     methods: {
       formatterIsPay(row, column, cellValue) {
         let status = Number.parseInt(cellValue) === 1 ? "是" : "否";
         return status;
       },
-      formatterIsLogistics(row, column, cellValue) {
+      formatterIsLogistics(row, column, cellValue) {       
         let num = Number.parseInt(cellValue);
-
+        
         switch(num) {
           case 0:
             return "配货";
@@ -94,30 +130,37 @@
             return '--';
             break;
         }
+        
       },
       formatterOrderStatus(row, column, cellValue) {
-        let num = Number.parseInt(cellValue);
+        let num = Number.parseInt(cellValue),
+          orderSratus = row.orderStatus;
 
-        switch(num) {
-          case 0:
-            return "配货";
-            break;
-          case 1:
-            return "运输中";
-            break;
-          case 2:
-            return "运输完毕";
-            break;
-          case 4:
-            return "退货中";
-            break;
-          case 5:
-            return "退货完毕";
-            break;
-          default:
-            return '--';
-            break;
+        if(orderSratus !== 4) {
+          switch(num) {
+            case 0:
+              return "配货";
+              break;
+            case 1:
+              return "运输中";
+              break;
+            case 2:
+              return "运输完毕";
+              break;
+            case 4:
+              return "退货中";
+              break;
+            case 5:
+              return "退货完毕";
+              break;
+            default:
+              return '--';
+              break;
+          }
+        }else {
+          return '';
         }
+        
       },  
       getData() {
         this.$http.post("agencyOrder/agencyOrderInfo", {
@@ -178,7 +221,106 @@
             type: "error"
           })
         })
-      }
+      },
+      handleTuiHuo: debounce(function(obj) {
+        let orderStatus = obj.orderStatus;
+
+        this.$http.post("https://www.jzwms.com/hnMiniApp/tuikuan", {
+          "agencyOrderID": this.detailID,
+          "refund": obj.orderAmount,
+          // "refund": 0.01,
+          "agencyOrderDetailID": obj.agencyOrderDetailID
+        }).then(data => {
+          console.log(data)
+          if (JSON.stringify(data) !== "{}") {
+            let result = data.data;
+            if (JSON.stringify(result) !== "{}") {
+              let status = result.status.toLowerCase();
+              console.log(status)
+              switch (status) {
+                case 'success':
+                  console.log(result)
+
+                  this.$messagebox.alert(orderStatus === 0 ? '退款成功' : '申请退货成功', '提示', {
+                    closeOnClickModal: false,
+                    type: "success",
+                    confirmButtonText: '确定',
+                    callback: action => {
+                      // this.$router.go(-1);
+                      // this.$router.push({
+                      //   name: 'AgencyOrderList'
+                      // })
+                      this.getData();
+                    }
+                  });
+
+
+                  break;
+                case 'failure':
+                  this.$message({
+                    showClose: true,
+                    message: '退款失败,请重试',
+                    duration: 0,
+                    type: "warning"
+                  })
+                  break;
+                case 'fail':
+                  this.$message({
+                    showClose: true,
+                    message: result.err_code_des,
+                    duration: 0,
+                    type: "warning"
+                  })
+                  break;
+                case 'null':
+                  this.$message({
+                    showClose: true,
+                    message: '退款无效',
+                    duration: 0,
+                    type: "warning"
+                  })
+                  break;
+                default:
+                  this.$message({
+                    showClose: true,
+                    message: '退款操作异常,请重试',
+                    duration: 0,
+                    type: "error"
+                  })
+                  break;
+              }
+
+            } else {
+              this.$message({
+                showClose: true,
+                message: '返回数据错误',
+                duration: 0,
+                type: "error"
+              })
+              return;
+            }
+          } else {
+            this.$message({
+              showClose: true,
+              message: '返回数据错误',
+              duration: 0,
+              type: "error"
+            })
+            return;
+          }
+
+        }).catch(error => {
+          console.log(error)
+          this.$message({
+            showClose: true,
+            message: error.message,
+            duration: 0,
+            type: "error"
+          })
+        })
+      }, 500, {
+      "maxWait": 1000
+      })
     },
     mounted() {
       this.detailID = this.$route.query.id;
